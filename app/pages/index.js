1be48df92de67cbe70375fdf5a48201786271fd6
calee14 <caplee1000@gmail.com>
2021-12-03 16:46:40 -0800

    added animations of harry potter guys

--- app/pages/index.js
+++ app/pages/index.js
@@ -29,7 +29,9 @@ export default function Home() {
         &nbsp;
       </div>
       <div className={styles.note1}>
-        <h1 data-aos="fade-left" className={styles.coverTitle}>I solemly swear that I am up to no good. Hehehe</h1>
+        <img data-aos="fade-left" data-aos-duration="1000" className={`${styles.potterCards} ${styles.dracoPic}`} src="https://pbs.twimg.com/media/EjLZyVzXsAENuyO.jpg"/>
+        <img data-aos="fade-up-right" data-aos-offset="5" data-aos-duration="500" className={`${styles.potterCards} ${styles.cedricPic}`} src="https://i.pinimg.com/originals/03/77/59/037759cd7db74ceb821bf0ad0139d04f.jpg"/>
+        <h1 data-aos="fade-up" data-aos-offset="10"  className={styles.coverTitle}>I solemly swear that I am up to no good. Hehehe</h1>
       </div>
     </div>
   )
